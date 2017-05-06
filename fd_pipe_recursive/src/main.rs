144ce8a9e86ab1d047031dc1fd05e8c8bf7dba46
hekrause <hekrause@htwg-konstanz.de>
2017-05-06 18:05:47 +0200

    Enough for Task3

--- fd_pipe_recursive/src/main.rs
+++ fd_pipe_recursive/src/main.rs
@@ -2,7 +2,7 @@
 extern crate nix;
 
 use nix::unistd::{fork, pipe, close, dup2, execvp, ForkResult};
-use nix::sys::wait::{waitpid, WCONTINUED};
+use nix::sys::wait::{waitpid, WEXITED};
 use std::os::unix::io::RawFd;
 use std::ffi::CString;
 
@@ -19,39 +19,35 @@ fn main() {
             argv.push(CString::new("-l").unwrap());
             let _result = execvp(&&argv[..][0], &argv[..]);
 
-            waitpid(child, Some(WCONTINUED));
+            waitpid(child, Some(WEXITED));
         }
         Ok(ForkResult::Child) => {
-            do_fork(writer0);
+            let (reader1, writer1) = pipe().unwrap();
+            match fork() {
+                Ok(ForkResult::Parent { child }) => {
+                    close(writer1).unwrap();
+                    dup2(writer0, 1);
+                    dup2(reader1, 0);
+
+                    let mut argv: Vec<CString> = Vec::new();
+                    argv.push(CString::new("grep").unwrap());
+                    argv.push(CString::new("test").unwrap());
+                    let _result = execvp(&&argv[..][0], &argv[..]);
+
+                    waitpid(child, Some(WEXITED));
+                }
+                Ok(ForkResult::Child) => {
+                    close(reader1);
+                    dup2(writer1, 1);
+                    let mut argv: Vec<CString> = Vec::new();
+                    argv.push(CString::new("cat").unwrap());
+                    argv.push(CString::new("test.txt").unwrap());
+                    let _result = execvp(&&argv[..][0], &argv[..]);
+                }
+                Err(_e) => {}
+            }
         }
         Err(_e) => {}
     }
 
-}
-
-fn do_fork(old_writer: RawFd) {
-    let (reader1, writer1) = pipe().unwrap();
-    match fork() {
-        Ok(ForkResult::Parent { child }) => {
-            close(writer1).unwrap();
-            dup2(old_writer, 1);
-            dup2(reader1, 0);
-
-            let mut argv: Vec<CString> = Vec::new();
-            argv.push(CString::new("grep").unwrap());
-            argv.push(CString::new("test").unwrap());
-            let _result = execvp(&&argv[..][0], &argv[..]);
-
-            waitpid(child, Some(WCONTINUED));
-        }
-        Ok(ForkResult::Child) => {
-            close(reader1);
-            dup2(writer1, 1);
-            let mut argv: Vec<CString> = Vec::new();
-            argv.push(CString::new("cat").unwrap());
-            argv.push(CString::new("test.txt").unwrap());
-            let _result = execvp(&&argv[..][0], &argv[..]);
-        }
-        Err(_e) => {}
-    }
 }
